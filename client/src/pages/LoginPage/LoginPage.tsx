f0272f6109d4978bfbe070ead7f7a7e59f757ea2
John Irle <irlejohn@gmail.com>
2021-09-21 01:50:27 -0500

    login page renders form

--- client/src/pages/LoginPage/LoginPage.tsx
+++ client/src/pages/LoginPage/LoginPage.tsx
@@ -4,16 +4,16 @@ import LoginForm from "../../components/LoginForm/LoginForm";
 import { logIn } from "../../actions/authenticationActions";
 
 const LoginPage = () => {
-  const dispatch = useDispatch();
-  const onSubmit = (email: string, password: string) => {
-    dispatch(logIn(email, password));
-  };
-  return (
-    <div>
-      <h1>Login</h1>
-      <LoginForm onSubmit={onSubmit} />
-    </div>
-  );
+    const dispatch = useDispatch();
+    const onSubmit = (email: string, password: string) => {
+        dispatch(logIn(email, password));
+    };
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginForm onSubmit={onSubmit} />
+        </div>
+    );
 };
 
 export default LoginPage;
