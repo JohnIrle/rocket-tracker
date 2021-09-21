0c634f35616996b177967cdedc74cc8c46057fec
John Irle <irlejohn@gmail.com>
2021-09-21 15:49:08 -0500

    add test handler

--- client/src/mocks/handlers.ts
+++ client/src/mocks/handlers.ts
@@ -2,8 +2,8 @@ import { rest } from "msw";
 import { loginHandler } from "./loginHandler";
 
 export const handlers = [
-  loginHandler,
-  rest.post("/api/users/currentuser", (req, res, ctx) => {
-    return res(ctx.status(200));
-  }),
+    loginHandler,
+    rest.post("/api/users/currentuser", (req, res, ctx) => {
+        return res(ctx.status(200));
+    }),
 ];
