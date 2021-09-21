a1db6a5336be13db774dfa74e25f2802d0306ba2
John Irle <irlejohn@gmail.com>
2021-09-21 15:49:08 -0500

    add test handler

--- client/src/mocks/handlers.ts
+++ client/src/mocks/handlers.ts
@@ -2,7 +2,12 @@ import { rest } from "msw";
 
 export const handlers = [
   rest.post("/api/users/signin", (req, res, ctx) => {
-    return res(ctx.status(200));
+    return res(
+      ctx.json({
+        firstName: "john",
+        lastName: "test",
+      })
+    );
   }),
 
   rest.post("/api/users/currentuser", (req, res, ctx) => {
