837126b03f08b3760e7f5672a871e643de371ceb
John Irle <irlejohn@gmail.com>
2021-09-22 16:25:50 -0500

    initial auth types

---
+++ client/src/actions/authenticationActionTypes.ts
@@ -0,0 +1,24 @@
+export const LOGIN_REQUEST = "LOGIN_REQUEST";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+
+export interface UserPayload {
+  email: string;
+  id: string;
+}
+
+export interface LoginRequest {
+  type: typeof LOGIN_REQUEST;
+}
+
+export interface LoginSuccess {
+  type: typeof LOGIN_SUCCESS;
+  payload: UserPayload;
+}
+
+export interface LoginFail {
+  type: typeof LOGIN_FAIL;
+  payload: string;
+}
+
+export type LoginTypes = LoginRequest | LoginSuccess | LoginFail;
