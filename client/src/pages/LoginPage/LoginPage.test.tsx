40e80e7a70a28a5cfc675089ac869b91cc41b6df
John Irle <irlejohn@gmail.com>
2021-09-22 17:21:24 -0500

    refactor to add provider

--- client/src/pages/LoginPage/LoginPage.test.tsx
+++ client/src/pages/LoginPage/LoginPage.test.tsx
@@ -1,18 +1,32 @@
 import { render, screen } from "@testing-library/react";
 import LoginPage from "./LoginPage";
+import { Provider } from "react-redux";
+import store from "../../store";
 
 it("renders", () => {
-  render(<LoginPage />);
+  render(
+    <Provider store={store}>
+      <LoginPage />
+    </Provider>
+  );
 });
 
 it("renders login heading", () => {
-  render(<LoginPage />);
+  render(
+    <Provider store={store}>
+      <LoginPage />
+    </Provider>
+  );
 
   expect(screen.getByRole("heading", { name: /login/i })).toBeInTheDocument();
 });
 
 it("renders the form", () => {
-  render(<LoginPage />);
+  render(
+    <Provider store={store}>
+      <LoginPage />
+    </Provider>
+  );
 
   expect(screen.getByRole("textbox", { name: /email:/i })).toBeInTheDocument();
   expect(screen.getByLabelText(/password:/i)).toBeInTheDocument();
