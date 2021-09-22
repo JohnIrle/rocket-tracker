93131406955a9df83ba5eb19993db90227728478
John Irle <irlejohn@gmail.com>
2021-09-22 16:25:29 -0500

    onSubmit now uses login action

--- client/src/pages/LoginPage/LoginPage.tsx
+++ client/src/pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import { logIn } from "../../actions/authenticationActions";
 
 const LoginPage = () => {
+  const dispatch = useDispatch();
   const onSubmit = (email: string, password: string) => {
-    console.log(email, password);
+    dispatch(logIn(email, password));
   };
   return (
     <div>
